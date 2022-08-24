0aafb7d439dde74d6e072d95e7f8abeaae68e402
UnStunned <kulkarnitheonly@gmail.com>
2022-08-24 12:08:03 +0530

    Changed structure

---
+++ Website/src/index.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+PORT = 8080;
+PATH = path.join(__dirname,"../public");
+
+app.use(express.static(PATH));
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port 8080");
+})
